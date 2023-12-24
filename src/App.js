63955594d74783c319c91d0b9882ab1b58936609
LimonsitOwO <luislm080304@gmail.com>
2023-12-23 20:25:05 -0500

    Prueba link

--- src/App.js
+++ src/App.js
@@ -59,9 +59,9 @@ const JobPage = () => {
 }
 
 function App() {
-  const location = useLocation();
+  const location = useLocation("");
 
-  if (location.pathname === '/' || location.pathname === '/workshop-3-bootcamp') {
+  if (location.pathname === 'https://noside44.github.io/' || location.pathname === 'https://noside44.github.io/workshop-3-bootcamp') {
     return <Navigate to="/home" />;
   }
   return (
